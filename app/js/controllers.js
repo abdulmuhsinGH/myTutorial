1d5f7abd143096cc6f7580952fe47dadaf349de7
BraKwame <ernestkuffour@gmail.com>
2014-09-08 00:32:30 +0000

    initial project files for step -4

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,22 +1,20 @@
 'use strict';
 
 /* Controllers */
-<<<<<<< HEAD
 var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', function ($scope) {
-=======
-
-var phonecatApp = angular.module('phonecatApp', []);
-
-phonecatApp.controller('PhoneListCtrl', function($scope) {
->>>>>>> c0d8c5fba819108a25fa1a1e3b7d3ef037d14ea6
   $scope.phones = [
     {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
+     'snippet': 'Fast just got faster with Nexus S.',
+     'age': 1},
     {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
+     'snippet': 'The Next, Next Generation tablet.',
+     'age': 2},
     {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
+     'snippet': 'The Next, Next Generation tablet.',
+     'age': 3}
   ];
+
+  $scope.orderProp = 'age';
 });
